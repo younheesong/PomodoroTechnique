9683f19eca13988312e490a7828a6f331beda3ff
younheesong <thd6517@naver.com>
2023-05-11 17:58:06 +0900

    design: css modfiy

--- app/layout/components/SidebarSection.tsx
+++ app/layout/components/SidebarSection.tsx
@@ -1,6 +1,6 @@
 const SidebarSection = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex flex-grow flex-col overflow-y-auto">
+    <div className="flex flex-grow flex-col overflow-y-auto relative">
       <nav className="mt-5 flex-1 space-y-1 px-2 pb-4">{children}</nav>
     </div>
   );
--- app/sidebar.tsx
+++ app/sidebar.tsx
@@ -1,25 +1,28 @@
+"use client";
 import NavItem from "./layout/components/NavItem";
 import SidebarSection from "./layout/components/SidebarSection";
 const Sidebar = () => {
   return (
-    <SidebarSection>
-      <NavItem to="/">
-        <NavItem.Icon iconName="ClipboardDocumentListIcon" />
-        <NavItem.Label label="To Dos" />
-      </NavItem>
-      <NavItem to="/">
-        <NavItem.Icon iconName="ViewColumnsIcon" />
-        <NavItem.Label label="Canvan" />
-      </NavItem>
-      <NavItem to="/">
-        <NavItem.Icon iconName="ClipboardDocumentCheckIcon" />
-        <NavItem.Label label="Completed Tasks" />
-      </NavItem>
-      <NavItem to="/">
-        <NavItem.Icon iconName="Cog6ToothIcon" />
-        <NavItem.Label label="Settings" />
-      </NavItem>
-    </SidebarSection>
+    <div className="w-[200px] relative hidden md:block">
+      <SidebarSection>
+        <NavItem to="/">
+          <NavItem.Icon iconName="ClipboardDocumentListIcon" />
+          <NavItem.Label label="To Dos" />
+        </NavItem>
+        <NavItem to="/">
+          <NavItem.Icon iconName="ViewColumnsIcon" />
+          <NavItem.Label label="Canvan" />
+        </NavItem>
+        <NavItem to="/">
+          <NavItem.Icon iconName="ClipboardDocumentCheckIcon" />
+          <NavItem.Label label="Completed Tasks" />
+        </NavItem>
+        <NavItem to="/">
+          <NavItem.Icon iconName="Cog6ToothIcon" />
+          <NavItem.Label label="Settings" />
+        </NavItem>
+      </SidebarSection>
+    </div>
   );
 };
 export default Sidebar;
