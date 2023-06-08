4ef3ca108495c303f2223679fdb5aa85c3511e33
younheesong <thd6517@naver.com>
2023-06-08 17:56:33 +0900

    fix: remove unused code

--- stores/store.tsx
+++
@@ -1,7 +0,0 @@
-import { create } from "zustand";
-
-import { ISidebarStates, createSidebarStore } from "./sidebar";
-// store 생성
-export const useSidebarStore = create<ISidebarStates>()((...a) => ({
-  ...createSidebarStore(...a),
-}));
