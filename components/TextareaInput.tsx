1da20bd49db417518b9be454efd7155459e3f65a
younheesong <thd6517@naver.com>
2023-06-21 10:16:21 +0900

    fix: css change

--- components/TextareaInput.tsx
+++ components/TextareaInput.tsx
@@ -6,7 +6,7 @@ const TextareaInput = forwardRef(function TextareaInput(
 ) {
   return (
     <textarea
-      className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus-within:ring-slate-200 sm:text-sm sm:leading-6"
+      className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus-within:ring-slate-200 text-sm sm:leading-6"
       {...props}
       ref={ref}
     />
