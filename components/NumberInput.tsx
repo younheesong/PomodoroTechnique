c3f6b35eb5ac09a8fc10094ef7d85623a63937f6
younheesong <thd6517@naver.com>
2023-06-21 10:16:36 +0900

    fix: add ref param

--- components/NumberInput.tsx
+++ components/NumberInput.tsx
@@ -1,10 +1,16 @@
-const NumberInput = (props: React.InputHTMLAttributes<HTMLInputElement>) => {
+import { forwardRef } from "react";
+
+const NumberInput = forwardRef(function NumberInput(
+  props: React.InputHTMLAttributes<HTMLInputElement>,
+  ref: any
+) {
   return (
     <input
       type="number"
       className="w-full border-0 bg-transparent py-1.5 text-white text-end focus:ring-0 sm:text-sm sm:leading-6"
       {...props}
+      ref={ref}
     />
   );
-};
+});
 export default NumberInput;
