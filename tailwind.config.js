919ad8f9c98ff9d6f37449f8393d1e651422f663
younheesong <thd6517@naver.com>
2023-06-22 17:01:39 +0900

    fix: bounce css modfiy

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,18 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    keyframes: {
+      bounce: {
+        "0%, 100%": {
+          transform: "translateY(-20%)",
+          "animation-timing-function": "cubic-bezier(0.8,0,1,1)",
+        },
+        "50%": {
+          transform: "none",
+          "animation-timing-function": "cubic-bezier(0,0,0.2,1)",
+        },
+      },
+    },
   },
   plugins: [require("@tailwindcss/forms")],
 };
