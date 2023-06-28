38377cb61bf4706c21e6e5615e2d60bc2c6ff56a
younheesong <thd6517@naver.com>
2023-06-28 10:46:31 +0900

    design: fix design

--- app/(site)/roadmaps/components/RoadmapSubMenu.tsx
+++ app/(site)/roadmaps/components/RoadmapSubMenu.tsx
@@ -8,15 +8,9 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/scrollbar";
-import "../../../../public/css/swiper.module.css";
-import { useState } from "react";
 import RoadmapSubMenuItem from "./RoadmapSubMenuItem";
 
 const RoadmapSubMenu = () => {
-  const [reachEnd, setReachEnd] = useState({
-    begin: false,
-    end: false,
-  });
   const breakpoints = {
     560: {
       slidesPerView: 5,
@@ -35,43 +29,17 @@ const RoadmapSubMenu = () => {
       spaceBetween: 10,
     },
   };
-  const arrowChange = (swiper) => {
-    if (swiper.isEnd) {
-      setReachEnd({
-        begin: false,
-        end: true,
-      });
-    } else if (swiper.realIndex === 0) {
-      setReachEnd({
-        end: false,
-        begin: true,
-      });
-    } else {
-      setReachEnd({
-        begin: false,
-        end: false,
-      });
-    }
-  };
 
   return (
     <div className="relative">
       <Swiper
-        // install Swiper modules
         className="text-white"
         modules={[Navigation]}
         spaceBetween={10}
-        slidesPerView={4}
-        // slidesPerGroup={3}
+        slidesPerView={4.6}
         navigation
         pagination={{ clickable: true }}
         scrollbar={{ draggable: true }}
-        onSlideChange={(swiper) => {
-          arrowChange(swiper);
-        }}
-        onSwiper={(e) => {
-          setReachEnd({ ...reachEnd, begin: true });
-        }}
         centeredSlidesBounds={true}
         breakpoints={breakpoints}
       >
